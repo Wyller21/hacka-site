8972fc5fd5f4f17acfb5c5f4e715f03bbe793922
Wyller Faria <giviam@hotmail.com>
2022-07-29 18:57:56 -0300

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./module.css"
+
+export function Index () {
+  return (
+
+    <div className={styles.body}>
+      <h1>Meu Index</h1>
+    </div>
+
+    
+  )
+
+}
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Index } from "../pages/index"
+
+function App () {
+  return (
+     <div>
+      <Index />
+     </div>
+      
+  
+  );
+}
+export default App;
